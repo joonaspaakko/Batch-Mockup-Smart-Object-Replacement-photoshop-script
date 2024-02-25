d535c78f221e28891ccc6090584a71bbebd10eba
Joonas Pääkkö <paakkoj@gmail.com>
2024-02-25 21:11:36 +0200

    Fixed an issue where using trimTransparency would fail processing the entire mockup
    It looks like I removed it at some point for no apparent reason...

--- script/Batch Mockup Smart Object Replacement.jsx
+++ script/Batch Mockup Smart Object Replacement.jsx
@@ -691,6 +691,9 @@ function replaceSoContents( item, sourcepath ) {
             
           }
           
+          soDoc.close( SaveOptions.DONOTSAVECHANGES );
+          if ( item.align ) returnLayer.remove();
+          
         }
         
         if ( item.resize ) {
